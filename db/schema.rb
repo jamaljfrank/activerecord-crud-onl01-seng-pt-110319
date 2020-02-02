e732e7a0f43439a74e7c1e4986fff52dd945ba49
Jamal Frank <jamaljfrank@gmail.com>
2020-02-02 14:40:07 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_02_132714) do
-
-  create_table "movies", force: :cascade do |t|
-    t.string "title"
-    t.datetime "release_date"
-    t.string "director"
-    t.string "lead"
-    t.boolean "in_theaters"
-  end
+ActiveRecord::Schema.define(version: 2020_02_02_125818) do
 
 end
